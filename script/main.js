33b5683a9164d8bce5f12ac1a7d5a0d6778063b3
sophn11 <sophianakpil@gmail.com>
2016-07-09 16:58:48 -0700

    used animate and animated the players

--- script/main.js
+++ script/main.js
@@ -1,4 +1,33 @@
-$(document).on("ready", function() {
 
-  
+$(document).on("ready", function() {
+    console.log ("sanity check");
+
+    $(document).on("keypress", function slothMove(event) {
+      if (event.which==115) {
+        $("#playerSloth").animate({left: "+=100"}, "fast");
+        console.log ("key s");
+      }
+    });
+
+    $(document).on("keypress", function bearMove(event) {
+      if (event.which==98) {
+        $("#playerBear").animate({left: "+=100"}, "fast");
+        console.log ("key b");
+      }
+    });
+
+    $(document).on("keypress", function koalaMove(event) {
+      if (event.which==107) {
+        $("#playerKoala").animate({left: "+=100"}, "fast");
+        console.log ("key k");
+      }
+    });
+
+    $(document).on("keypress", function monkeyMove(event) {
+      if (event.which==54) {
+        $("#playerMonkey").animate({left: "+=100"}, "fast");
+        console.log ("key 6");
+      }
+    });
+
 });
