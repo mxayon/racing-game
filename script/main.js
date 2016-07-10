4689994c71c1aa55ebfd548390d9edf51a64c91b
sophn11 <sophianakpil@gmail.com>
2016-07-09 20:30:01 -0700

    added win function and called back on each player function

--- script/main.js
+++ script/main.js
@@ -1,46 +1,70 @@
 
 $(document).on("ready", function() {
   $('.jumbotron').hide();
-  var counter = 0;
+  var slothCount = 0;
+  var bearCount = 0;
+  var koalaCount = 0;
+  var monkeyCount = 0;
+  var winCount = 0;
     console.log ("sanity check");
 
 function win (playerWinner) {
       $( '.jumbotron' ).show( 15 );
       $('.winning').text(playerWinner + " " + "JUST WON!");
+      winCount++;
 }
     $(document).on("keypress", function slothMove(event) {
-      if (counter >= 65) {
+      if ((slothCount >= 65) || (winCount ===1)) {
         console.log("sloth wins!");
         ////stops sloth from happening
       } else if (event.which==115) {
         $("#playerSloth").animate({left: "+=10"}, 100);
-        counter ++;
-        console.log(counter);
-        if (counter === 65) {
+        slothCount ++;
+        console.log(slothCount);
+        if (slothCount === 65) {
           win("Sloth");
+
         }
         }
       });
 
     $(document).on("keypress", function bearMove(event) {
-      if (event.which==98) {
+      if ((bearCount >= 65) || (winCount ===1)) {
+        console.log("bear wins!");
+      } else if (event.which==98) {
         $("#playerBear").animate({left: "+=10"}, 100);
-        console.log ("key b");
+        bearCount ++;
+        console.log (bearCount);
+        if (bearCount === 65) {
+          win ("Bear");
+        }
       }
     });
 
     $(document).on("keypress", function koalaMove(event) {
-      if (event.which==107) {
+      if ((koalaCount >= 65) || (winCount ===1)) {
+        console.log("koala wins!");
+      } else if (event.which==107) {
         $("#playerKoala").animate({left: "+=10"}, 100);
-        console.log ("key k");
+        koalaCount ++;
+        console.log (koalaCount);
+        if (koalaCount === 65) {
+          win ("Koala");
+        }
       }
     });
 
     $(document).on("keypress", function monkeyMove(event) {
-      if (event.which==54) {
+      if ((monkeyCount >= 65) || (winCount ===1)) {
+        console.log("monkey wins!");
+      } else if (event.which==54) {
         $("#playerMonkey").animate({left: "+=10"}, 100);
-        console.log ("key 6");
+        monkeyCount ++;
+        console.log (monkeyCount);
+        if (monkeyCount === 65) {
+          win ("Monkey");
       }
+     }
     });
 
 /////reset button ////
